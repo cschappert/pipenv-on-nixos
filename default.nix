c7daf410a619f4abe72fcdc8891b042e51393e7d
Chris Schappert <work.nonwork+gh@gmail.com>
2019-02-09 23:16:21 +0900

    initial commit

---
+++ default.nix
@@ -0,0 +1,36 @@
+with import <nixpkgs> {};
+with pkgs.python36Packages;
+
+stdenv.mkDerivation {
+  name = "Python36WithPipenv";
+  buildInputs = [
+
+    # Install Python 3.6 and it's pip package from nixpkgs.
+    # Python36Packages.pip and pipenv-2018.7.1 (current in nixpkgs) 
+    # are incompatible so we install pipenv into the user dir with pip
+    # and modify PATH, PYTHONPATH in the shellHook.  
+    
+    python36Full
+    python36Packages.pip
+
+    # The following packages are related to the dependencies of your python
+    # project. (C libs to compile against, etc.)
+
+    stdenv
+    zlib
+
+  ];
+
+  src = null;
+
+  shellHook = ''
+ 
+    # set SOURCE_DATE_EPOCH so that we can use python wheels
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    PATH=/home/chris/.local/bin:$PATH
+    PYTHONPATH=/home/chris/.local/lib/python3.6/site-packages:$PYTHONPATH
+    pip install --user pipenv
+
+  '';
+}
